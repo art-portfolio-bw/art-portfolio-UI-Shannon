a20f8475cbac9cfb383e5be965fe079fd3978354
Shannon <shannon.yoshikawa@gmail.com>
2019-05-21 16:20:44 -0700

    yet again, still working on nav

--- components/navigation/nav.js
+++ components/navigation/nav.js
@@ -1,17 +1,17 @@
-$( document ).ready(function() { //checks of doc is ready to be manipulated
-    var hamburger = $('#hamburger-icon');
-    hamburger.click(function() {    //when clicked, toggles to active class
-       hamburger.toggleClass('active');
-       return false;
-    });
-  });
+// $( document ).ready(function() { //checks of doc is ready to be manipulated
+//     var hamburger = $('#hamburger-icon');
+//     hamburger.click(function() {    //when clicked, toggles to active class
+//        hamburger.toggleClass('active');
+//        return false;
+//     });
+//   });
 
-// const mobileMenu = document.querySelector('.nav')
+// const mobileMenu = document.querySelector('nav')
 // const toggleMenu = ()=>{
 //     mobileMenu.classList.toggle('menu--open')
 // }
 
-// const ham = document.querySelector('#hamburger-icon')
+// const ham = document.querySelector('.icon')
 
 // ham.addEventListener('click', toggleMenu)
  
